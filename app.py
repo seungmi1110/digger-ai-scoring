4d8811e3e7a30149a44043b9abe492b04e2bac04
dP-fl <87458730+dP-fl@users.noreply.github.com>
2025-02-03 08:31:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.set_page_config(layout="wide")
 # 섹션 표시 여부를 토글할 수 있도록 설정
 sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
-# 네비게이션 로드 (섹션 포함 여부에 따라 파일 변경)
+# 네비게이션 로드
 nav = get_nav_from_toml(
-    "streamlit/pages_sections.toml" if sections else "streamlit/pages.toml"
+    "streamlit/pages.toml"
 )
 
 # 네비게이션
